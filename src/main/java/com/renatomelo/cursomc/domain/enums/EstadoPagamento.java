2ed4127e959bba58d41e32f8e093de95f46c5d2b
Renato Melo <renato.olimelo@gmail.com>
2025-06-13 11:59:26 -0300

    Inicio de Pagamento e Pedido

--- src/main/java/com/renatomelo/cursomc/domain/enums/EstadoPagamento.java
+++ src/main/java/com/renatomelo/cursomc/domain/enums/EstadoPagamento.java
@@ -1,13 +1,13 @@
 package com.renatomelo.cursomc.domain.enums;
 
-public enum TipoCliente {
+public enum EstadoPagamento {
 
-	PESSOAFISICA(1, "Pessoa Física"), PESSOAJURIDICA(2, "Pessoa Jurídica");
+	PENDENTE(1, "Pendente"), QUITADO(2, "Quitado"), CANCELADO(3, "Cancelado");
 
 	private int cod;
 	private String descricao;
 
-	private TipoCliente(int cod, String descricao) {
+	private EstadoPagamento(int cod, String descricao) {
 		this.cod = cod;
 		this.descricao = descricao;
 	}
@@ -20,13 +20,13 @@ public enum TipoCliente {
 		return descricao;
 	}
 
-	public static TipoCliente toEnum(Integer cod) {
+	public static EstadoPagamento toEnum(Integer cod) {
 		if (cod == null) {
 			return null;
 		}
-		for (TipoCliente tc : TipoCliente.values()) {
-			if (cod.equals(tc.getCod())) {
-				return tc;
+		for (EstadoPagamento ep : EstadoPagamento.values()) {
+			if (cod.equals(ep.getCod())) {
+				return ep;
 			}
 		}
 		throw new IllegalArgumentException("Id inválido: " + cod);
