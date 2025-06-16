2da522250a1fd586b0c97b3c42c3b576ee33d4dd
Renato Melo <renato.olimelo@gmail.com>
2025-06-16 17:56:41 -0300

    Criando metodo de insercao de Categoria

--- src/main/java/com/renatomelo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renatomelo/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,15 @@
 package com.renatomelo.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renatomelo.cursomc.domain.Categoria;
 import com.renatomelo.cursomc.services.CategoriaService;
@@ -23,6 +28,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
