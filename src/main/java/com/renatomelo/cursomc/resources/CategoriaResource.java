fd08171b2e88f3a257edc7a09ad53b890b014158
Renato Melo <renato.olimelo@gmail.com>
2025-07-09 11:08:40 -0300

    Listando Categorias com DTO

--- src/main/java/com/renatomelo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renatomelo/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.renatomelo.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renatomelo.cursomc.domain.Categoria;
+import com.renatomelo.cursomc.dto.CategoriaDTO;
 import com.renatomelo.cursomc.services.CategoriaService;
 
 @RestController
@@ -50,6 +53,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/renatomelo/cursomc/services/CategoriaService.java
+++ src/main/java/com/renatomelo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.renatomelo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -42,6 +43,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
