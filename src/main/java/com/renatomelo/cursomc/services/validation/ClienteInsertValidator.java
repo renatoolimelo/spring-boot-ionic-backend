46fa797b733c6f9e119b93c5bba9b8af75874238
Renato Melo <renato.olimelo@gmail.com>
2025-07-15 10:29:25 -0300

    Validacao customizada emai nao repetido na insercao de cliente

--- src/main/java/com/renatomelo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/renatomelo/cursomc/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,10 @@ package com.renatomelo.cursomc.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.renatomelo.cursomc.domain.Cliente;
 import com.renatomelo.cursomc.domain.enums.TipoCliente;
 import com.renatomelo.cursomc.dto.ClienteNewDTO;
+import com.renatomelo.cursomc.repositories.ClienteRepository;
 import com.renatomelo.cursomc.repositories.exception.FieldMessage;
 import com.renatomelo.cursomc.services.validation.utils.BR;
 
@@ -12,6 +14,12 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	private ClienteRepository clienteRepository;
+
+	public ClienteInsertValidator(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
 
 	@Override
 	public void initialize(ClienteInsert clienteInsert) {
@@ -25,10 +33,16 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				&& !BR.isValidCPF(clienteNewDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
+
 		if (clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
 				&& !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		Cliente clienteEmail = clienteRepository.findByEmail(clienteNewDTO.getEmail());
+		
+		if(clienteEmail != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado!"));
 		}
 
 		for (FieldMessage fieldMessage : list) {
